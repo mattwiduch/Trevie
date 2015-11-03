226a13ea8c04684c64e137d158fdbb3a5758d326
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-03 20:16:44 +0000

    Adds MovieItem class and custom ArrayAdapter to display it on the grid

--- app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
@@ -1,13 +1,16 @@
 package com.redraysoftware.trevie;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,10 +23,19 @@ public class MovieGridFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //placeholder items
+        List<MovieItem> movieItems = new ArrayList<MovieItem>();
+        for (int i = 0; i < 33; i++) {
+            MovieItem item = new MovieItem();
+            movieItems.add(item);
+        }
+
         View view = inflater.inflate(R.layout.movie_grid_fragment, container, false);
 
         GridView gridView = (GridView) view.findViewById(R.id.movie_grid);
-        gridView.setAdapter(new MoviePosterAdapter(view.getContext()));
+        gridView.setAdapter(new MovieItemAdapter(getActivity(), R.layout.movie_grid_item, movieItems));
+
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
@@ -31,6 +43,7 @@ public class MovieGridFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
         return view;
     }
 }
---
+++ app/src/main/java/com/redraysoftware/trevie/MovieItem.java
@@ -0,0 +1,16 @@
+package com.redraysoftware.trevie;
+
+/**
+ * Created by frano on 03/11/2015.
+ */
+public class MovieItem {
+    private Integer mPosterId;
+
+    public MovieItem() {
+        mPosterId = R.drawable.temp;
+    }
+
+    public int getPosterId() {
+        return mPosterId;
+    }
+}
