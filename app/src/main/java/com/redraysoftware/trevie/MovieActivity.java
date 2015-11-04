ef674bca3b510636c875094a07460d07e9d6be68
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-04 17:56:43 +0000

    Adds view bindings in MovieActivity

--- app/src/main/java/com/redraysoftware/trevie/MovieActivity.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieActivity.java
@@ -3,16 +3,28 @@ package com.redraysoftware.trevie;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MovieActivity extends AppCompatActivity {
+    @Bind(R.id.movie_details_title) TextView titleTextView;
+    @Bind(R.id.movie_details_release_date) TextView releaseTextView;
+    @Bind(R.id.movie_details_rating) TextView ratingTextView;
+    @Bind(R.id.movie_details_synopsis) TextView synopsisTextView;
+    @Bind(R.id.movie_details_poster) ImageView posterImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
