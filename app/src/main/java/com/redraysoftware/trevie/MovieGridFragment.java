0f933ab0fbb2898aecef6159ffa0de262c95f2d4
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-07 22:27:15 +0000

    Add data retrieval code as well as internet permission

--- app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
@@ -13,6 +13,8 @@ import android.widget.GridView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -98,9 +100,37 @@ public class MovieGridFragment extends Fragment {
                         .build();
 
                 URL url = new URL(builtUri.toString());
-                Log.v(TAG, "URI " + builtUri.toString());
 
-                //https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=74e771b1880fb81a691ffe79e949489e
+                // Create request and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    //Nothing to do
+                    return null;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty
+                    return null;
+                }
+
+                moviesJsonString = buffer.toString();
+                Log.v(TAG, moviesJsonString);
 
             } catch (IOException e){
                 Log.e(TAG, "Error ", e);
