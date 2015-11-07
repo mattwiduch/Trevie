bea1a3cb74d2a5f40fb46191c89b7dd96f6599ff
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-07 22:11:32 +0000

    Adds API query URL builder

--- app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
@@ -1,6 +1,7 @@
 package com.redraysoftware.trevie;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.widget.GridView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +30,13 @@ public class MovieGridFragment extends Fragment {
     private List<Movie> mMovieList;
 
     public MovieGridFragment() {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+        fetchMoviesTask.execute("sort");
     }
 
     @Override
@@ -72,6 +81,26 @@ public class MovieGridFragment extends Fragment {
             String moviesJsonString = null;
 
             try {
+                // Construct query URL
+                final String TMDB_BASE_URL = "https://api.themoviedb.org/3";
+                final String DISCOVER_PATH = "discover";
+                final String MOVIE_PATH = "movie";
+                final String SORT_PARAM = "sort_by";
+                final String VOTE_COUNT_PARAM = "vote_count.gte";
+                final String API_KEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(TMDB_BASE_URL).buildUpon()
+                        .appendPath(DISCOVER_PATH)
+                        .appendPath(MOVIE_PATH)
+                        .appendQueryParameter(SORT_PARAM, "vote_average.desc")
+                        .appendQueryParameter(VOTE_COUNT_PARAM, "100")
+                        .appendQueryParameter(API_KEY_PARAM, BuildConfig.OPEN_THE_MOVIEDB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+                Log.v(TAG, "URI " + builtUri.toString());
+
+                //https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=74e771b1880fb81a691ffe79e949489e
 
             } catch (IOException e){
                 Log.e(TAG, "Error ", e);
