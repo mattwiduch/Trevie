3c8fbe2a31c9c7b4ff9d0d7a58917ca43dc7e0c3
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-09 19:31:42 +0000

    feat: Add Sort Icon to the AppBar
    It will allow the user to sort movies by selected sort type.

--- app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieGridFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -35,6 +38,27 @@ public class MovieGridFragment extends Fragment {
     private MovieDetailsAdapter mMovieDetailsAdapter;
 
     public MovieGridFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_grid, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
