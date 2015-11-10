b5bd25d0e30d45e1ef646150a77581e105dbd80b
Mateusz Widuch <matt.widuch@gmail.com>
2015-11-10 18:34:45 +0000

    feat: Improve grid item layout by adding details

--- app/src/main/java/com/redraysoftware/trevie/MovieDetailsAdapter.java
+++ app/src/main/java/com/redraysoftware/trevie/MovieDetailsAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -24,22 +25,42 @@ public class MovieDetailsAdapter extends ArrayAdapter<Movie> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        Movie movie = getItem(position);
+        //ImageView imageView;
+        ViewHolder viewHolder;
 
         if (convertView == null) {
             // inflate the GridView item layout
             LayoutInflater inflater = LayoutInflater.from(mContext);
             convertView = inflater.inflate(R.layout.movie_grid_item, parent, false);
             //set views
-            imageView = (ImageView) convertView.findViewById(R.id.movie_grid_poster);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            //imageView = (ImageView) convertView.findViewById(R.id.movie_grid_poster);
+            //imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            viewHolder = new ViewHolder();
+            viewHolder.movieTitle = (TextView) convertView.findViewById(R.id.grid_movie_title);
+            viewHolder.movieGenre = (TextView) convertView.findViewById(R.id.grid_movie_genre);
+            viewHolder.moviePoster = (ImageView) convertView.findViewById(R.id.grid_movie_poster);
+            viewHolder.moviePoster.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            viewHolder.favouriteButton = (ImageView) convertView.findViewById(R.id.grid_movie_favourite);
+            convertView.setTag(viewHolder);
         } else {
-            imageView = (ImageView) convertView;
+            //imageView = (ImageView) convertView;
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
+
+        // Update view
+        Movie movie = getItem(position);
         Picasso.with(mContext).load(movie.getPosterPath()).placeholder(R.drawable.temp)
-                .error(R.drawable.error).fit().into(imageView);
-        return imageView;
+                .error(R.drawable.error).fit().into(viewHolder.moviePoster);
+        viewHolder.movieTitle.setText(movie.getTitle());
+        viewHolder.movieTitle.setSelected(true);
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView movieTitle;
+        TextView movieGenre;
+        ImageView moviePoster;
+        ImageView favouriteButton;
     }
 }
