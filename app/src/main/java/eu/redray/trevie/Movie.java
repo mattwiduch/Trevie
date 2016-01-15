35425956ff270921fe9369d35fa1a4bb440d4f04
Mateusz Widuch <matt.widuch@gmail.com>
2016-01-15 21:32:00 +0000

    Add additional fields to Movie class
    These will be used to store data related to movie length, genre and country of
    origin.

--- app/src/main/java/eu/redray/trevie/Movie.java
+++ app/src/main/java/eu/redray/trevie/Movie.java
@@ -7,10 +7,13 @@ package eu.redray.trevie;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.List;
+
 /**
  * Represents movie item.
  */
 public class Movie implements Parcelable {
+
     public static final String EXTRA_DETAILS = "movie_details";
     public static final Creator<Movie> CREATOR = new Creator<Movie>() {
         @Override
@@ -23,26 +26,40 @@ public class Movie implements Parcelable {
             return new Movie[size];
         }
     };
+
+    private String mId;
     private String mTitle;
     private String mReleaseDate;
     private String mRating;
     private String mSynopsis;
     private String mPosterPath;
+    private String mRuntime;
+    private List<String> mGenres;
+    private List<String> mCountries;
 
-    public Movie(String title, String releaseDate, String avgRating, String overview, String posterPath) {
+    public Movie(String id, String title, String releaseDate, String avgRating, String overview, String posterPath,
+                 String runtime, List<String> genres, List<String> countries) {
+        mId = id;
         mTitle = title;
         mReleaseDate = releaseDate;
         mRating = avgRating;
         mSynopsis = overview;
         mPosterPath = posterPath;
+        mRuntime = runtime;
+        mGenres = genres;
+        mCountries = countries;
     }
 
     protected Movie(Parcel in) {
+        mId = in.readString();
         mTitle = in.readString();
         mReleaseDate = in.readString();
         mRating = in.readString();
         mSynopsis = in.readString();
         mPosterPath = in.readString();
+        mRuntime = in.readString();
+        mGenres = in.readArrayList(String.class.getClassLoader());
+        mCountries = in.readArrayList(String.class.getClassLoader());
     }
 
     @Override
@@ -52,11 +69,15 @@ public class Movie implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mId);
         dest.writeString(mTitle);
         dest.writeString(mReleaseDate);
         dest.writeString(mRating);
         dest.writeString(mSynopsis);
         dest.writeString(mPosterPath);
+        dest.writeString(mRuntime);
+        dest.writeList(mGenres);
+        dest.writeList(mCountries);
     }
 
     public String getPosterPath() {
@@ -81,5 +102,17 @@ public class Movie implements Parcelable {
 
     public String getReleaseYear() {
         return mReleaseDate.substring(0, 4);
+    }
+
+    public String getRuntime() {
+        return mRuntime;
+    }
+
+    public List<String> getGenres() {
+        return mGenres;
+    }
+
+    public List<String> getCountries() {
+        return mCountries;
     }
 }
