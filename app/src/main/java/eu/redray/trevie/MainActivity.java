843420b7a98c5352d65902a849aa2138695d0709
Mateusz Widuch <matt.widuch@gmail.com>
2016-01-23 23:57:31 +0000

    feat: Disable Stetho debug bridge

--- app/src/main/java/eu/redray/trevie/MainActivity.java
+++ app/src/main/java/eu/redray/trevie/MainActivity.java
@@ -11,8 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import static com.facebook.stetho.Stetho.initializeWithDefaults;
-
 /**
  * Starts the application.
  */
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements MovieGridFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Initialise Stetho debug bridge
-        initializeWithDefaults(this);
+        //initializeWithDefaults(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
