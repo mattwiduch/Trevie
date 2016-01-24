50b410b5b7e0d4c89926b49d62fdcb9da9a8ee40
Mateusz Widuch <matt.widuch@gmail.com>
2016-01-24 17:28:00 +0000

    refactor: Simplify YouTubeUri class

--- app/src/main/java/eu/redray/trevie/utility/YouTubeUri.java
+++ app/src/main/java/eu/redray/trevie/utility/YouTubeUri.java
@@ -1,22 +1,22 @@
+/*
+ * Copyright (C) 2016 Mateusz Widuch
+ */
 package eu.redray.trevie.utility;
 
 import android.net.Uri;
 
 /**
- * Created by frano on 16/01/2016.
+ * Creates YouTube Uri from given key value
  */
 public class YouTubeUri {
-
     final static String YT_BASE_URL = "https://www.youtube.com";
     final static String WATCH_PATH = "watch";
     final static String VIDEO_PARAM = "v";
 
     public static Uri create(String key) {
-        Uri uri = Uri.parse(YT_BASE_URL).buildUpon()
+        return Uri.parse(YT_BASE_URL).buildUpon()
                 .appendPath(WATCH_PATH)
                 .appendQueryParameter(VIDEO_PARAM, key)
                 .build();
-
-        return uri;
     }
 }
