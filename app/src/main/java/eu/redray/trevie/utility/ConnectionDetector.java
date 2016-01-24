087c511e5521237b13c8e72950c470bf57885575
Mateusz Widuch <matt.widuch@gmail.com>
2016-01-24 22:03:48 +0000

    refactor: Use non deprecated methods to check for connection on APIs 21+

--- app/src/main/java/eu/redray/trevie/utility/ConnectionDetector.java
+++ app/src/main/java/eu/redray/trevie/utility/ConnectionDetector.java
@@ -5,24 +5,46 @@ package eu.redray.trevie.utility;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 
 /**
  * Checks for all possible internet providers
  */
 public class ConnectionDetector {
-    /** Returns true if internet connection is available. */
-    public static boolean isInternetConnectionAvailable(Context context){
-        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivity != null)
-        {
-            NetworkInfo[] info = connectivity.getAllNetworkInfo();
-            if (info != null)
-                for (NetworkInfo anInfo : info)
-                    if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+    /**
+     * Returns true if internet connection is available.
+     */
+    public static boolean isInternetConnectionAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Use when API => 21
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            if (connectivityManager != null) {
+                Network[] networks = connectivityManager.getAllNetworks();
+                NetworkInfo networkInfo;
+                for (Network network : networks) {
+                    networkInfo = connectivityManager.getNetworkInfo(network);
+                    if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
                         return true;
                     }
+                }
+            }
         }
+        // Use when API < 21
+        else {
+            if (connectivityManager != null) {
+                @SuppressWarnings("deprecation")
+                NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+                if (info != null) {
+                    for (NetworkInfo anInfo : info)
+                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            return true;
+                        }
+                }
+            }
+        }
+
         return false;
     }
 }
